51df31217b21c6fab81ad984c33db1664e262389
Claudio <claudio@karyon.com.br>
2024-12-16 17:57:49 -0300

    feat: aumentando range de produtos

--- src/utils/getProducts.ts
+++ src/utils/getProducts.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export async function getProducts() {
-  const url = 'https://sheets.googleapis.com/v4/spreadsheets/14f4xK_L6C25Meyvf6wWgekD5_xxLac-z0tTV0PrYjNQ/values/2:29?key=AIzaSyBk89LG2STO_hBF9LyIipLcwXK4YOYqEsM';
+  const url = 'https://sheets.googleapis.com/v4/spreadsheets/14f4xK_L6C25Meyvf6wWgekD5_xxLac-z0tTV0PrYjNQ/values/2:99?key=AIzaSyBk89LG2STO_hBF9LyIipLcwXK4YOYqEsM';
   
   try {
     // Fazendo a requisição para a URL
